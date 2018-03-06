0cfe3e79671d3446c8adcdd35c2ac036867c9f22
parthmittal <parth15069@iiitd.ac.in>
2018-03-06 20:55:49 +0530

    added basic driver to test brandes
    TODO: replace with fancy input reader thing

--- bwc/main.cpp
+++ bwc/main.cpp
@@ -1,8 +1,30 @@
+#include <iomanip>
 #include <iostream>
 
-/* headers from ../include */
+#include <brandes.hpp>
 #include <graph.hpp>
 
-/* headers from ./include */
+int main() {
+    using namespace std;
+    using namespace our;
 
-int main() {}
+    /* read graph from STDIN, write betweenness centrality to STDOUT */
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<int>> adj(N);
+    for (int i = 0; i < M; ++i) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    graph_t<int> graph(adj);
+
+    cout << fixed << setprecision(6);
+    for (const double &d : brandes::bwc_all(graph)) {
+        cout << d << endl;
+    }
+}
