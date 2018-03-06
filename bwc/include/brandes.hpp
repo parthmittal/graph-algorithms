5d32e553fa9a8f36f53e47dd8c4f172ba7131da9
parthmittal <parth15069@iiitd.ac.in>
2018-03-06 20:40:23 +0530

    imported brandes implementation from og

--- bwc/include/brandes.hpp
+++ bwc/include/brandes.hpp
@@ -1,10 +1,15 @@
+#include <vector>
+
 #include <graph.hpp>
 
 #ifndef __BRANDES_HPP_
 #define __BRANDES_HPP_
 
 namespace our {
-namespace brandes {}
+namespace brandes {
+void bwc1(const graph_t<int> &G, int source, std::vector<double> &BC);
+std::vector<double> bwc_all(const graph_t<int> &G);
+} // namespace brandes
 } // namespace our
 
 #endif
