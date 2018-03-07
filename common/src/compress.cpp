3257c1f710ede6543becf13c71a63c09db8a6d52
parthmittal <parth15069@iiitd.ac.in>
2018-03-07 18:56:11 +0530

    fixed some bugs

--- common/src/compress.cpp
+++ common/src/compress.cpp
@@ -4,9 +4,7 @@ namespace our {
 std::map<int, int> compress_list(const std::set<int> &data, int &offset) {
     std::map<int, int> key;
     for (auto &i : data) {
-        if (key.find(i) == key.end()) {
-            key[i] = offset++;
-        }
+        key[i] = offset++;
     }
     return key;
 }
--- common/src/graph_reader.cpp
+++ common/src/graph_reader.cpp
@@ -1,53 +1,47 @@
-#include <compress.hpp>
 #include <fstream>
-#include <graph.hpp>
-#include <graph_reader.hpp>
 #include <map>
 #include <set>
+#include <sstream>
 #include <string>
+
+#include <compress.hpp>
+#include <graph.hpp>
+#include <graph_reader.hpp>
 
 namespace our {
 naive_graph_t<int> read_file(std::string filename, OptParser parser) {
-    std::vector<std::vector<int>> adj;
+    /* it's safe to use namespace within a function */
+    using namespace std;
+
     int vertex_count = 0;
     int edge_count = 0;
-    ifstream input_graph(filenme);
+    ifstream input_graph(filename);
     if (input_graph.is_open()) {
         string text_line;
         if (parser.checkIncluded("vertex_count")) {
-            std::getline(input_graph, text_line);
-            std::istringstream iss(text_line);
-            std::vector<std::string> splitted_line(
-                std::isteam_iterator<std::string>{iss},
-                std::istream_iterator<std::string>());
-            vertex_count = std::stoi(splitted_line[0], nullptr);
-            edge_count = std::stoi(splitted_line[1], nullptr);
-        } else {
-            std::set<int> vertex_list;
-            std::vector<pair<int, int>> edge_list;
-            while (std::getline(input_graph, text_line)) {
-                std::istringstream iss(text_line);
-                std::vector<std::string> splitted_line(
-                    std::istream_iterator<std::string>{iss},
-                    std::istream_iterator<std::string>());
-                u = std::stoi(splitted_line[0], nullptr);
-                v = std::stoi(splitted_line[1], nullptr);
-                vertex_list.insert(u);
-                vertex_list.insert(v);
-                edge_list.push_back({u, v});
-                edge_count += 1;
-            }
+            input_graph >> vertex_count >> edge_count;
         }
+
+        set<int> vertex_list;
+        vector<pair<int, int>> edge_list;
+
+        int u, v;
+        while (input_graph >> u >> v) {
+            vertex_list.insert(u);
+            vertex_list.insert(v);
+            edge_list.push_back({u, v});
+        }
+
         int offset = 0;
-        std::map<int, int> key = compress_list(vertex_list, offset);
-        vertex_count = offset;
-        adj.resize(vertex_count);
+        map<int, int> key = compress_list(vertex_list, offset);
+        vertex_count = vertex_list.size();
+        vector<vector<int>> adj(vertex_count);
         for (auto &e : edge_list) {
             adj[key[e.first]].push_back(key[e.second]);
             if (!parser.checkIncluded("is_directed"))
                 adj[key[e.second]].push_back(key[e.first]);
         }
-        return { adj, vertex_count, edge_count }
+        return {adj, vertex_count, edge_count};
     }
 }
 } // namespace our
