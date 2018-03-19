69c4aba6beda366e7945a2b285e3828840d46677
Kushagra Arora <kushagra15049@iiitd.ac.in>
2018-03-20 04:17:19 +0530

    K bfs implemented. Complete. Yet to be tested.

---
+++ cc/src/k_bfs.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <cstdlib>
+#include <queue>
+#include <set>
+#include <vector>
+
+#include <graph.hpp>
+
+/*
+ * A standard bfs implementation which updates the dist vector so as to find the
+ * max distance from a set of vertices
+ * source is the source of the bfs
+ * root represents the kth time for which this bfs is run
+ * dist is vector of pairs where second value represents the distance from the
+ * current node while the first value represents the max distance from the
+ * sources from which bfs is
+ */
+template <typename graph_type>
+void bfs(int source, int root, const graph_type &G,
+         std::vector<pair<int, int>> &dist, vector<__int128> visited) {
+    dist[source].second = 0;
+    dist[source].first = std::max(dist[source].first, dist[source].second);
+    visited[source] = visited[v] | (1 << root);
+    queue<int> q;
+    q.push(source);
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (auto &i : G[u]) {
+            if (!(visited[i] & (1 << root))) {
+                dist[i].second = dist[u].second + 1;
+                dist[i].first = std::max(dist[i].second, dist[i].first);
+                visited[i] = visited[i] | (1 << root);
+                q.push(i);
+            }
+        }
+    }
+}
+
+template <typename graph_type>
+std::vector<int> k_bfs(const graph_type &G, int k, int N) {
+    srand((int)time(0));
+    std::vector<__int128> visited(N, 0);
+    std::vector<pair<int, int>> dist(N, {0, inf});
+
+    /* generate k distinct vertices */
+    std::set<int> k_vertices;
+    while (k_vertices.size() < k) {
+        int a = (rand() % n);
+        k_vertices.insert(a);
+    }
+
+    /* PHASE 1
+     * run bfs from each of the k_vertices and keep a track of max distance from
+     * these vertices */
+    int root = 0;
+    for (auto &v : k_vertices) {
+        bfs(v, root++, G, dist, visited);
+    }
+
+    int max_distance = std::max_element(dist.begin(), dist.end())->first;
+
+    /* find the set of farthest nodes from the initial random sets */
+    std::vector<int> farthest;
+    for (int i = 0; i < N; i++) {
+        if (dist[i].first == max_distance) {
+            farthest.push_back(i);
+        }
+    }
+
+    /* PHASE 2
+     * running bfs from the set of farthest vertices and maintaining max
+     * distance
+     */
+    std::fill(visited.begin(), visited.end(), 0);
+    int root = 0;
+    for (auto &i : farthest) {
+        bfs(i, root++, G, dist, visited);
+    }
+    std::vector<int> eccentricity(N);
+    for (int i = 0; i < N; i++) {
+        eccentricity[i] = dist[i].first;
+    }
+    return eccentricity;
+}
