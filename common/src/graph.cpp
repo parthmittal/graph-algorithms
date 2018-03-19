133c945ef08c5900a824f6548c6f315602aac88f
Kushagra Arora <kushagra15049@iiitd.ac.in>
2018-03-20 04:18:40 +0530

    added the largest WCC structure

---
+++ common/src/graph.cpp
@@ -0,0 +1,28 @@
+#include <stack>
+
+#include <graph.hpp>
+
+template<typename edge_t>
+void our::LWCC_t<edge_t>::iterative_dfs(int source) {
+	std::stack<int> S;
+    while (!S.empty()) {
+        int u = S.top();
+        S.pop();
+        if(visited[u])
+        	continue;
+        visited[u] = true;
+        n++;
+
+        for (int i = graph_t<edge_t>::si[u]; i < graph_t<edge_t>::ei[u]; i++) {
+            if (!visited[graph_t<edge_t>::crs[i]]) {
+                S.push(graph_t<edge_t>::crs[i]);
+            }
+        }
+    }
+}
+
+template<typename edge_t>
+bool our::LWCC_t<edge_t>::in_LWCC(int i) { return visited[i]; }
+
+template<typename edge_t>
+int our::LWCC_t<edge_t>::size_LWCC() { return n; }
