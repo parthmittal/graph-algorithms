451ec272036e5b58ea7424c2528faa24c856b341
Kushagra Arora <kushagra15049@iiitd.ac.in>
2018-03-26 13:10:12 +0530

    up to date

--- bwc/main.cpp
+++ bwc/main.cpp
@@ -2,7 +2,24 @@
 
 /* headers from ../include */
 #include <graph.hpp>
+#include <graph_reader.hpp>
+#include <optparser.hpp>
 
 /* headers from ./include */
 
-int main() {}
+using namespace std;
+
+int main(int argc, const char **argv) {
+    our::OptParser parser;
+    parser.addOption("-v", "--vertex_count");
+    parser.addOption("-d", "--is_directed");
+    parser.addOption("-a", "--some_random_option");
+    parser.parse(argc, argv);
+    int v = parser.checkIncluded("vertex_count");
+    int d = parser.checkIncluded("is_directed");
+    int a = parser.checkIncluded("a");
+    auto graph = our::read_file(
+        "/home/kushagra/Desktop/6/IP/graph-algorithms/Email-Enron.txt", parser);
+    cout << "Vertex count in Enron: " << graph.vertex_count << endl;
+    return 0;
+}
