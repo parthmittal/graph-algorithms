d4ba9efced51c2cd9c700beecec38d2b654016f8
parthmittal <parth15069@iiitd.ac.in>
2018-04-17 17:38:14 +0530

    sdm: construct skeleton graph

---
+++ bwc/include/skeleton_sdm.hpp
@@ -0,0 +1,41 @@
+#include <vector>
+
+#include <graph.hpp>
+
+#ifndef __SKELETON_SDM_HPP__
+#define __SKELETON_SDM_HPP__
+
+namespace sdm {
+
+struct wm_edge_t {
+    int v, weight;
+    long long mult; /* multiplicity of edge */
+};
+
+struct sk_hack_t {
+    std::vector<std::vector<wm_edge_t>> adj;
+    std::vector<int> id, rid;
+
+    const our::graph_t<int> &G;
+    const std::vector<int> &P; /* P[i] <- partition id of  i */
+    std::vector<std::vector<int>>
+        part; /* part[i] <- vector containing all x s.t P[x] = i */
+
+    /* we need these to do BFS efficiently; they represent nothing in final
+     * graph */
+    std::vector<int> dist;
+    std::vector<long long> mult;
+
+    sk_hack_t(const our::graph_t<int> &G, const std::vector<int> &P);
+    void add_sp_edges(int source);
+    void add_vertex(int v);
+};
+
+struct sk_graph_t : sk_hack_t, graph<wm_edge_t> {
+    sk_graph_t(const graph_t<int> &G, const std::vector<int> &P)
+        : sk_hack_t(G, P), graph<wm_edge_t>(adj) {}
+};
+
+} // namespace sdm
+
+#endif
---
+++ bwc/src/skeleton_sdm.cpp
@@ -0,0 +1,82 @@
+#include <vector>
+#include <queue>
+
+#include <graph.hpp>
+#include <skeleton_sdm.hpp>
+
+using namespace std;
+
+namespace sdm {
+sk_hack_t::sk_hack_t(const our::graph_t<int> &G, const vector<int> &P)
+    : P(P), G(G), id(G.N, -1) {
+    for (int u = 0; u < G.N; ++u) {
+        for (const int &v : G[u]) {
+            if (P[u] != P[v]) { /* belong to different partitions */
+                add_vertex(u);
+                add_vertex(v);
+                adj[id[u]].push_back({id[v], 1, 1});
+            }
+        }
+    }
+
+    for (int u = 0; u < G.N; ++u) {
+        part[P[u]].push_back(u);
+    }
+
+    for (int u = 0; u < G.N; ++u) {
+        if (id[u] != -1) {
+            add_sp_edges(u);
+        }
+    }
+}
+
+void sk_hack_t::add_vertex(int v) {
+    if (id[v] == -1) {
+        rid.push_back(v);
+        id[v] = adj.size();
+        adj.push_back({});
+    }
+}
+
+void sk_hack_t::add_sp_edges(int source) {
+    typedef long long ll;
+
+    int pid = P[source];
+
+    for (auto &v : part[pid]) {
+        dist[v] = G.N + 1;
+        mult[v] = 0;
+    }
+
+    queue<int> bfq;
+    bfq.push(source);
+    dist[source] = 0;
+    mult[source] = 1;
+
+    while(!bfq.empty()) {
+        int u = bfq.front();
+        bfq.pop();
+        for (const int &v : G[u]) {
+            if (P[v] != pid) {
+                continue;
+            }
+            if (dist[v] > dist[u] + 1) {
+                dist[v] = dist[u] + 1;
+                mult[v] = 0;
+                if (id[v] == -1) {
+                    bfq.push(v);
+                }
+            }
+            if (dist[v] == dist[u] + 1) {
+                mult[v] += mult[u];
+            }
+        }
+    }
+
+    for (const int &v : part[source]) {
+        if (dist[v] < G.N + 1) {
+            adj[source].push_back({v, dist[v], mult[v]});
+        }
+    }
+}
+}; // namespace sdm
