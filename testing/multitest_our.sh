e427047465da8855e8879a44a288643636332cd5
parthmittal <parth15069@iiitd.ac.in>
2018-06-29 15:55:13 +0530

    made output of multitest way prettier
    h/t: top comment on https://stackoverflow.com/a/3330834

--- testing/multitest_our.sh
+++ testing/multitest_our.sh
@@ -5,7 +5,20 @@
 # NB: __don't__ use this in the directory testing in the repo root, instead
 # use the copy created by make in Release/testing.
 
-echo USAGE: num_tests num_vertices p
+sp="/-\|"
+sc=0
+spin() {
+   printf "\b${sp:sc++:1}"
+   ((sc==${#sp})) && sc=0
+}
+endspin() {
+   printf "\r%s\n" "$@"
+}
+
+if [ "$#" -ne 3 ]; then 
+    echo USAGE: num_tests num_vertices p
+    exit 1
+fi
 
 TRUE="../bwc/bwc_exec -b"
 CHECK="../bwc/bwc_exec"
@@ -13,16 +26,21 @@ TESTS=$1
 N=$2
 p=$3
 
-for i in {1..$TESTS}
-do
+for ((i=1; i<=$TESTS; i++)); do
+    spin
     ./generator $N $p > graph.txt
     ./l2cc < graph.txt > graph.l2cc
-    $TRUE < graph.l2cc > true
-    $CHECK < graph.l2cc > check
+    $TRUE < graph.l2cc > true 2> /dev/null
+    $CHECK < graph.l2cc > check 2> /dev/null
     if ./rcmp6 graph.l2cc check true 2> diffout ; then
-        echo "Test [$i] ok"
+        # test passed
+        :
     else
+        endspin
         echo "Test [$i] FAILED, check graph.txt, diffout"
         exit 1
     fi
 done
+
+endspin
+echo "all tests OK"
